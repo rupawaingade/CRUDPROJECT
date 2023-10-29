83c8874ac6f9454e81538c6153b8ab5401815dde
Rupa Waingade <rupa.waingade@gmail.com>
2023-10-28 20:06:07 -0400

    Restore

--- CRUDPROJECT/asgi.py
+++ CRUDPROJECT/asgi.py
@@ -7,10 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-"""
-Test comment
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
